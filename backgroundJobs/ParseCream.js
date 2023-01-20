3855b66e0a7a226b4dd57dced1ef9d690e588ff7
Alfulinku <9356022+alfulinku@users.noreply.github.com>
2023-01-20 16:36:37 +0100

    Update ParseCream.js

--- backgroundJobs/ParseCream.js
+++ backgroundJobs/ParseCream.js
@@ -1,6 +1,5 @@
 const axios = require('axios');
 const Addresses = require("./Addresses.js")
-const Compound = require("./CompoundParser")
 const BSCParser = require("./BSCParser.js")
 const Web3 = require("web3")
 require('dotenv').config()
@@ -11,18 +10,15 @@ class CreamParser extends BSCParser {
         const compoundInfo = Addresses.creamAddress
         const network = 'BSC'
         const web3 = new Web3(process.env.BSC_NODE_URL)
-        super(compoundInfo, network, web3, 24 * 5)
+        super(compoundInfo, network, web3, 24, 1, 'cream_BSC_users.json')
     }
 }
 
 module.exports = { Parser: CreamParser}
 
 // async function test() {
-//     const Web3 = require("web3")
-//     const web3 = new Web3("https://bsc-dataseed1.defibit.io/")    
-
-//     const comp = new VenusParser(Addresses.venusAddress, "BSC", web3)
-//     await comp.main()    
+//   const parser = new CreamParser();
+//   await parser.main();
 // }
 
-// test()
+// test();
